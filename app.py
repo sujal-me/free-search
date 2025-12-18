d5078c19aaa37929878c80e9ef16bf46fa7c8e3c
sujal-me <9harrypotter2004@gmail.com>
2025-12-18 20:07:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -276,5 +276,6 @@ async def health():
 
 if __name__ == "__main__":
     import uvicorn
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run(app, host="0.0.0.0", port=port)
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
